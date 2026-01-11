f6cd0addcdff900bba00cd7ff7d91ec28cf6853a
Junyoung Oh <ordinarydennis@gmail.com>
2026-01-11 19:41:51 +0900

    Resolve LeetCode75 Solution - Array / String - 1071. Greatest Common Divisor of Strings(fail)

--- LeetCode75/Array_String/1071_Greatest_Common_Divisor_of_Strings.cpp
+++ LeetCode75/Array_String/1071_Greatest_Common_Divisor_of_Strings.cpp
@@ -1,31 +1,39 @@
 class Solution {
+
+	bool check(string& str, string& base)
+	{
+		if(str.size() % base.size() != 0)
+			return false;
+		
+		string s;
+		
+		while(s.size() < str.size())
+		{
+			 s += base;
+		}
+		return s == str;
+	}
+	
+	
 public:
-    bool valid(string str1, string str2, int k) {
-        int len1 = str1.size(), len2 = str2.size();
-        if (len1 % k > 0 || len2 % k > 0) {
-            return false;
-        } else {
-            string base = str1.substr(0, k);
-            int n1 = len1 / k, n2 = len2 / k;
-            return str1 == joinWords(base, n1) && str2 == joinWords(base, n2);
-        }
-    }
-    string joinWords(string str, int k) {
-        string ans = "";
-        for (int i = 0; i < k; ++i) {
-            ans += str;
-        }
-        return ans;
-    }
-    
-    
     string gcdOfStrings(string str1, string str2) {
-        int len1 = str1.length(), len2 = str2.length();
-        for (int i = min(len1, len2); i >= 1; --i) {
-            if (valid(str1, str2, i)) {
-                return str1.substr(0, i);
-            }
-        }
-        return "";
+     
+		string ret;
+		
+		int min = std::min(str1.size(), str2.size());
+		
+		while(min)
+		{
+			string base = str2.substr(0, min);
+			
+			if(check(str1, base) && check(str2, base))
+			{
+				return base;
+			}
+			min--;
+		}
+
+		return ret;
     }
 };
+
