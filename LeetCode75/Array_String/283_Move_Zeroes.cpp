cf0e87c35c10a596451286c0e0cd7d43d4a507df
Junyoung Oh <ordinarydennis@gmail.com>
2026-01-17 18:08:26 +0900

    Add LeetCode75 Solution Read Editorial - Two Pointers - 283. Move Zeroes

--- LeetCode75/Array_String/283_Move_Zeroes.cpp
+++ LeetCode75/Array_String/283_Move_Zeroes.cpp
@@ -1,3 +1,54 @@
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+		
+		vector<int> ret(nums.size(), 0);
+		
+		int index = 0;
+		for(int i = 0; i < nums.size(); i++)
+		{
+			if(nums[i])
+			{
+				ret[index++] = nums[i];
+			}
+		}
+		
+		nums = ret;
+    }
+};
+
+class Solution {
+public:
+  void moveZeroes(vector<int>& nums) {
+      int lastNonZeroFoundAt = 0;
+      // If the current element is not 0, then we need to
+      // append it just in front of last non 0 element we found.
+      for (int i = 0; i < nums.size(); i++) {
+          if (nums[i] != 0) {
+              nums[lastNonZeroFoundAt++] = nums[i];
+          }
+      }
+    // After we have finished processing new elements,
+    // all the non-zero elements are already at beginning of array.
+    // We just need to fill remaining elements in the array with 0's.
+      for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+          nums[i] = 0;
+      }
+  }
+};
+
+class Solution {
+public:
+  void moveZeroes(vector<int>& nums) {
+      for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+          if (nums[cur] != 0) {
+              swap(nums[lastNonZeroFoundAt++], nums[cur]);
+          }
+      }
+  }
+};
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -36,3 +87,4 @@ public:
 		}
     }
 };
+
